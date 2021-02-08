9f0d56a64b841bb91dd4a9d73eb6b5e398b81efc
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-02-08 21:29:09 +0300

    repair cli

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,4 @@ import readlineSync from 'readline-sync';
 export default () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  const herr = 'kukusik';
 };
