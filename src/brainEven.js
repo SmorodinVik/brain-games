26cff5b6462dcc7359503384b34f733779c64dd7
Viktor <53858778+SmorodinaVik@users.noreply.github.com>
2021-02-09 23:18:41 +0300

    Update brainEven.js

--- src/brainEven.js
+++ src/brainEven.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
-import isEven from 'isEven.js';
-import getRandomNumber from 'getRandomNumber.js';
+import isEven from './isEven.js';
+import getRandomNumber from './getRandomNumber.js';
 
 export default () => {
   const userName = readlineSync.question('May I have your name? ');
