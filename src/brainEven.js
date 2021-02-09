0a03c0a92009792ad2ffac5c57e2b24fae9fb587
Viktor <53858778+SmorodinaVik@users.noreply.github.com>
2021-02-09 23:13:36 +0300

    Create brainEven.js

---
+++ src/brainEven.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+import isEven from 'isEven.js';
+import getRandomNumber from 'getRandomNumber.js';
+
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let questionCount = 0;
+  do {
+    const number = getRandomNumber();
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`,
+      );
+      return;
+    }
+    console.log('Correct!');
+    questionCount += 1;
+  } while (questionCount !== 3);
+  console.log(`Congratulations, ${userName}!`);
+  return;
+};
